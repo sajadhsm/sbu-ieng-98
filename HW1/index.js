06b7a41dd362bcc704784a7232c815ddd2eb8e34
Sajad Hashemian <sajadhsm@gmail.com>
2019-10-25 17:51:53 +0330

    Fix PORT wrong case

--- HW1/index.js
+++ HW1/index.js
@@ -4,7 +4,7 @@ const db = require('./src/db');
 const routes = require('./src/routes');
 const { readJSONFileSync } = require('./src/utils');
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
