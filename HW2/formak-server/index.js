fc106ff7b2c69565ce6372f16a1748234d626335
Sajad Hashemian <sajadhsm@gmail.com>
2019-12-19 20:30:48 +0330

    Prepare to deploy on Heroku

--- HW2/formak-server/index.js
+++ HW2/formak-server/index.js
@@ -9,15 +9,12 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../formak-client/build')));
 
 app.use('/api/v1/', routes);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../formak-client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
