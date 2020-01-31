a3f42c28b15dbd8fc18169a17b8a0bdd45a1ab9d
Sajad Hashemian <sajadhsm@gmail.com>
2020-01-31 16:34:11 +0330

    feat(server): Enable CORS

--- Project/server/index.js
+++ Project/server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
 
@@ -10,6 +11,7 @@ const routes = require('./src/routes');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use('/api/v1/', routes);
