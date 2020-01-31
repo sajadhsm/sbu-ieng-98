f45dfb4176540f313c04c3e999c0acb2e1c63003
Sajad Hashemian <sajadhsm@gmail.com>
2020-01-31 05:42:37 +0330

    feat(server):  Add POST & DELETE methods for Form

--- Project/server/src/models/Form.js
+++ Project/server/src/models/Form.js
@@ -2,13 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const formSchema = new Schema({
-  id: Schema.Types.ObjectId,
   title: String,
   fields: [
     {
       name: String,
       title: String,
-      type: String,
+      type: { type: String },
       required: Boolean
     }
   ],
--- Project/server/src/routes/forms.js
+++ Project/server/src/routes/forms.js
@@ -19,4 +19,39 @@ router.get("/", (req, res) => {
     });
 });
 
+router.post("/", (req, res) => {
+  const { title, fields } = req.body;
+
+  const form = new Form({
+    title,
+    fields
+  });
+
+  form
+    .save()
+    .then(result => {
+      res.status(status.CREATED).json(result);
+    })
+    .catch(error => {
+      winston.error(error);
+      res.status(status.INTERNAL_SERVER_ERROR).json({
+        error
+      });
+    });
+});
+
+router.delete("/:formId", (req, res) => {
+  Form.remove({ _id: req.params.formId })
+    .exec()
+    .then(result => {
+      res.status(status.OK).json(result);
+    })
+    .catch(error => {
+      winston.error(error);
+      res.status(status.INTERNAL_SERVER_ERROR).json({
+        error
+      });
+    });
+});
+
 module.exports = router;
