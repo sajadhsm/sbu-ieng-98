875862d71ba3d6171d6ac48ccd5fea81e702d8a6
Sajad Hashemian <sajadhsm@gmail.com>
2020-02-08 20:44:44 +0330

    fix(server): Support select option in Form model

--- Project/server/src/models/Form.js
+++ Project/server/src/models/Form.js
@@ -2,12 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const formSchema = new Schema({
-  title: String,
+  title: {
+    type: String,
+    required: true
+  },
   fields: [
     {
       name: String,
       title: String,
       type: { type: String },
+      options: [
+        {
+          text: String,
+          value: Schema.Types.Mixed
+        }
+      ],
       required: Boolean
     }
   ],
